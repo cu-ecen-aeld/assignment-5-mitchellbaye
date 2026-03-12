ca87b99b4954b93387ebf6c1aa10cad32e496926
Mitchell <mitchell.baye@lovibond.us>
2026-03-11 22:57:50 -0600

    Assignment 5 Part 2 full_build commit

--- base_external/package/aesd-socketApp/aesd-socketApp.mk
+++ base_external/package/aesd-socketApp/aesd-socketApp.mk
@@ -6,26 +6,20 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 2ce6537114d5290c50fbdbb0fc0f002f9fea95c4
+AESD_SOCKETAPP_VERSION = 2ce6537114d5290c50fbdbb0fc0f002f9fea95c4
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-mitchellbaye
-AESD_ASSIGNMENTS_SITE_METHOD = git
-AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+AESD_SOCKETAPP_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-mitchellbaye
+AESD_SOCKETAPP_SITE_METHOD = git
+AESD_SOCKETAPP_GIT_SUBMODULES = YES
 
-define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) CC=$(TARGET_CC) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+define AESD_SOCKETAPP_BUILD_CMDS
+	$(MAKE) CC=$(TARGET_CC) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
-define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin/
-	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/
+define AESD_SOCKETAPP_INSTALL_TARGET_CMDS
     $(INSTALL) -m 0775 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/
     $(INSTALL) -m 0775 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
